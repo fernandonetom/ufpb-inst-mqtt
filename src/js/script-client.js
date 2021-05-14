5f4cf090ab7706a2947981f6e4a4ae48c08d6572
fernando39357 <fernando39357@gmail.com>
2021-05-13 21:36:25 -0300

    first version

---
+++ src/js/script-client.js
@@ -0,0 +1,100 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const client = mqtt.connect('ws://broker.mqttdashboard.com:8000/mqtt');
+  client.subscribe('mqtt/ufpb-inst-test');
+
+  client.on('message', function (topic, payload) {
+    try {
+      const number = parseFloat(payload.toString());
+      updateChart(number);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+  client.on('connect', () => {
+    setTimeout(infoHide, 1000);
+  });
+  // setInterval(() => {
+  //   client.publish(
+  //     'mqtt/ufpb-inst-test',
+  //     (Math.floor(Math.random() * 6) + 1).toString()
+  //   );
+  // }, 2000);
+
+  let config = {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: 'Temperatura',
+          backgroundColor: '#42a5f5',
+          borderColor: '#42a5f5',
+          data: [],
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+      scales: {
+        y: {
+          suggestedMin: 0,
+          suggestedMax: 50,
+        },
+      },
+      interaction: {
+        mode: 'nearest',
+        intersect: false,
+      },
+    },
+  };
+
+  const ctx = document.getElementById('canvas').getContext('2d');
+  window.myLine = new Chart(ctx, config);
+
+  const logs = document.getElementById('logs');
+
+  const limit = 10;
+
+  const updateChart = (number) => {
+    if (config.data.datasets.length > 0) {
+      const date = new Date();
+      const hours = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      const copyData = config.data.labels.slice((limit - 1) * -1);
+
+      copyData.push(hours);
+      config.data.labels = copyData;
+
+      config.data.datasets.forEach(function (dataset) {
+        const copyData = dataset.data.slice((limit - 1) * -1);
+        copyData.push(number);
+
+        logs.innerHTML += `${hours} - Recebeu ${number} ºC<br />`;
+
+        dataset.data = copyData;
+      });
+
+      window.myLine.update();
+    }
+  };
+  infoShow(
+    '<h3>Conectando </h3><img width="100px" src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" />'
+  );
+  function infoShow(text) {
+    const info = document.getElementById('info');
+    const canvas = document.getElementById('canvas');
+    info.innerHTML = text;
+    info.style.display = 'flex';
+    canvas.style.display = 'none';
+  }
+  function infoHide() {
+    const info = document.getElementById('info');
+    const canvas = document.getElementById('canvas');
+    info.style.display = 'none';
+    canvas.style.display = 'block';
+  }
+});
---
+++ src/js/script-sensor.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const client = mqtt.connect('ws://broker.mqttdashboard.com:8000/mqtt');
+
+  client.on('connect', () => {
+    setTimeout(infoHide, 1000);
+  });
+  infoShow(
+    '<h3>Conectando </h3><img width="100px" src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" />'
+  );
+
+  function infoShow(text) {
+    const info = document.getElementById('info');
+    const content = document.getElementById('content');
+    info.innerHTML = text;
+    info.style.display = 'flex';
+    content.style.display = 'none';
+  }
+  function infoHide() {
+    const info = document.getElementById('info');
+    const content = document.getElementById('content');
+    info.style.display = 'none';
+    content.style.display = 'flex';
+  }
+
+  const tempInput = document.getElementById('temp');
+  const valueLabel = document.getElementById('value');
+  const button = document.querySelector('button');
+  const logs = document.getElementById('logs');
+  let value = tempInput.value;
+
+  tempInput.addEventListener('change', (e) => {
+    value = e.target.value;
+    valueLabel.innerHTML = `${value} ºC`;
+  });
+  button.addEventListener('click', () => {
+    try {
+      client.publish('mqtt/ufpb-inst-test', value.toString());
+      const date = new Date();
+      logs.innerHTML += `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} - Enviou ${value} ºC <br />`;
+    } catch (error) {}
+  });
+});
